749fa0a67ffa6ea4cda4ca27f6d912b00266200b
Mothana <snakeeaterrdev@gmail.com>
2024-03-18 15:55:00 +0400

    fix

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ function handleSubmit(e) {
     subject: this.subject.value,
     describeProject: this.description.value
   };
-  fetch('https://192.153.57.22:3000/sendToIFTTT', {
+  fetch('https://snakeeater.info:3000/sendToIFTTT', {
     method: 'POST',
     body: JSON.stringify(body),
     headers: {
